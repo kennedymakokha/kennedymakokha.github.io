5331e6644927bfe7b848aa8d3020b213b2ecb964
kennedymakokha <katchibo2@gmail.com>
2019-08-29 10:05:55 +0300

    Updates

---
+++ precache-manifest.4c2099bf22376cfeca021b79e9ee7b15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130a91e39216fd98262f1a485f275c03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b9e10b05b15c69ce33",
+    "url": "/static/css/main.d4595263.chunk.css"
+  },
+  {
+    "revision": "5862095b00463a57e1fa",
+    "url": "/static/js/2.8f0b4515.chunk.js"
+  },
+  {
+    "revision": "32b9e10b05b15c69ce33",
+    "url": "/static/js/main.45b7f5c8.chunk.js"
+  },
+  {
+    "revision": "edf77e470791c6b64f1c",
+    "url": "/static/js/runtime~main.0be9e7ee.js"
+  },
+  {
+    "revision": "c74ae4a724151675cc286ca86bd3b4de",
+    "url": "/static/media/kennedy.c74ae4a7.jpg"
+  },
+  {
+    "revision": "47924c5b150ca3e8dc532fa3d97811d5",
+    "url": "/static/media/laravel.47924c5b.png"
+  },
+  {
+    "revision": "6a34904f5d2a4ed699ea6e1a6a1dcff0",
+    "url": "/static/media/mycv.6a34904f.docx"
+  },
+  {
+    "revision": "10f090a7dd726442a5edea71d9c8315f",
+    "url": "/static/media/stats-bg.10f090a7.jpg"
+  }
+]);
