e0597433bedeb113ee2a9eb12d9166d23deaaf73
kennedymakokha <katchibo2@gmail.com>
2019-09-06 12:17:55 +0300

    sending mail

--- src/components/contact.js
+++ src/components/contact.js
@@ -40,7 +40,7 @@ class contact extends Component {
         }
         console.log(emailBody)
 
-        await axios.post(`http://104.248.247.251:7000/contact/send`, emailBody)
+        await axios.post('http://104.248.247.251:7000/contact/send', emailBody)
             .then(res => {
                 this.setState({
                     name: '',
